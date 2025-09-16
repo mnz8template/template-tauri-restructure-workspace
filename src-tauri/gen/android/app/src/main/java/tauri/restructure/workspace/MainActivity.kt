0f212849d9c6d2d4260fad69c8cd1f2bfc52992f
mnz8 <mnz8gg@gmail.com>
2025-09-17 02:16:56 +0800

    feat: restructure

---
+++ src-tauri/gen/android/app/src/main/java/tauri/restructure/workspace/MainActivity.kt
@@ -0,0 +1,11 @@
+package tauri.restructure.workspace
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
